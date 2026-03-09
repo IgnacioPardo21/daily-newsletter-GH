b8b619656de334bdfddf5ac54d9e40f0a6649c3c
IgnacioPardo21 <ignaciopardo@kpmg.es>
2026-03-09 20:30:37 +0100

    Implement email newsletter feature
    Added functionality to send a daily newsletter via email with news articles.

--- script/newsletter.py
+++ script/newsletter.py
@@ -1,7 +1,11 @@
 import requests
 import os
+import smtplib
+from email.mime.text import MIMEText
 
 API_KEY = os.environ.get("NEWS_API_KEY")
+EMAIL_USER = os.environ.get("EMAIL_USER")
+EMAIL_PASS = os.environ.get("EMAIL_PASS")
 
 topics = [
     "real estate España",
@@ -16,6 +20,8 @@ topics = [
     "innovación empresarial España"
 ]
 
+newsletter = ""
+
 for topic in topics:
 
     url = f"https://newsapi.org/v2/everything?q={topic}&language=es&pageSize=3&apiKey={API_KEY}"
@@ -23,8 +29,20 @@ for topic in topics:
     response = requests.get(url)
     data = response.json()
 
-    print(f"\n=== {topic.upper()} ===")
+    newsletter += f"\n\n=== {topic.upper()} ===\n"
 
     if "articles" in data:
         for article in data["articles"][:3]:
-            print("-", article["title"])
+            newsletter += "- " + article["title"] + "\n"
+
+msg = MIMEText(newsletter)
+msg["Subject"] = "Tu newsletter diaria"
+msg["From"] = EMAIL_USER
+msg["To"] = EMAIL_USER
+
+server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+server.login(EMAIL_USER, EMAIL_PASS)
+server.send_message(msg)
+server.quit()
+
+print("Email enviado")
